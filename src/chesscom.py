3bc282c55db0abf571865cfe619df096eef2f6dc
Neel Gore <57605901+neelgore@users.noreply.github.com>
2020-08-31 20:44:43 -0700

    added W-D-L

--- src/chesscom.py
+++ src/chesscom.py
@@ -4,36 +4,46 @@ from datetime import datetime
 
 def get_recent_tpr(username: str, games: int, time_control: str):
 
-    def tpr_of_game(pgn: PGN, username: str) -> int:
+    def tpr_of_game(pgn: PGN, username: str) -> (int, int):
         if pgn.tags["White"] == username:
-            if pgn.game.rstrip("\n").endswith("1/2-1/2"):
-                return int(pgn.tags["BlackElo"])
-            elif pgn.game.rstrip("\n").endswith("1-0"):
-                return int(pgn.tags["BlackElo"]) + 400
+            if pgn.tags["Result"] == "1/2-1/2":
+                return (int(pgn.tags["BlackElo"]), 0)
+            elif pgn.tags["Result"] == "1-0":
+                return (int(pgn.tags["BlackElo"]) + 400, 1)
             else:
-                return int(pgn.tags["BlackElo"]) - 400
+                return (int(pgn.tags["BlackElo"]) - 400, -1)
         else:
             if pgn.game.rstrip("\n").endswith("1/2-1/2"):
-                return int(pgn.tags["WhiteElo"])
+                return (int(pgn.tags["WhiteElo"]), 0)
             elif pgn.game.rstrip("\n").endswith("0-1"):
-                return int(pgn.tags["WhiteElo"]) + 400
+                return (int(pgn.tags["WhiteElo"]) + 400, 1)
             else:
-                return int(pgn.tags["WhiteElo"]) - 400
+                return (int(pgn.tags["WhiteElo"]) - 400, -1)
 
         
     now = str(datetime.now())
     pgns = get_chesscom_games(username, now[0:4], now[5:7])
     count = 0
     elo_sum = 0
+    wins = 0
+    losses = 0
     for pgn in pgns:
         if pgn.tags["TimeControl"] == time_control:
             count += 1
-            elo_sum += tpr_of_game(pgn, username)
+            result = tpr_of_game(pgn, username)
+            elo_sum += result[0]
+            if result[1] == 1:
+                wins += 1
+            elif result[1] == -1:
+                losses += 1
             if count == games: break
-    return elo_sum/count
+    draws = games - wins - losses
+    return (elo_sum/count, wins, draws, losses)
             
 def run():
-    print("TPR is:", round(get_recent_tpr(input("chess.com username:\n"), int(input("number of games:\n")), input("time control:\n"))))
+    answer = get_recent_tpr(input("chess.com username:\n"), int(input("number of games:\n")), input("time control:\n"))
+    print("Performance rating is:", round(answer[0]))
+    print(f"{answer[1]} wins, {answer[2]} draws, {answer[3]} losses")
 
 if __name__ == "__main__":
     run()
